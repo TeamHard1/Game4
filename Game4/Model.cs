d13f20df4910ecbda050b2a0b85ea0e1ae35cc3b
user <user@C1011>
2022-04-01 10:16:50 +0300

    1

---
+++ Game4/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game4
+{
+    internal class Model
+    {
+        public static class IModel
+        {
+            int[] array = new array[0, 1];
+        }
+    }
+}
